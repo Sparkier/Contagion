89b94dbbc77a19bbbece8871545c6a8f033bedd0
Alex <alex.baeuerle@uni-ulm.de>
2020-03-20 22:09:24 +0100

    Initial Commit of the Project
    Initial setup of the Repo.

---
+++ Contagion_iOS/Contagion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Contagion
+//
+//  Created by Alex Bäuerle on 20.03.20.
+//  Copyright © 2020 Alex Bäuerle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Text("Hello, World!")
+            Text("Hello, World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
